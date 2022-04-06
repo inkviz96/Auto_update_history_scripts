5ec464d273552ac58606980f10623aed14ba7df2
Semyon <39240102+inkviz96@users.noreply.github.com>
2022-04-06 17:16:12 +0300

    Update start.py

--- start.py
+++ start.py
@@ -6,7 +6,6 @@ from huoby_update_history import huoby_update_history
 
 if __name__ == '__main__':
 	logging.info('start update')
-	for i in [bsc_update_history, huoby_update_history]:
-		thread = threading.Thread(target=i)
-		thread.start()
+	thread = threading.Thread(target=huoby_update_history)
+	thread.start()
 
